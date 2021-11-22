75fcb0c7c09e21ae8ddbd9f7b8c6d601be4e58c6
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2021-11-21 20:33:19 -0500

    "finished" autonomous
    arbitrary numbers

--- Autonomous.java
+++ Autonomous.java
@@ -13,6 +13,9 @@ public class vroom extends LinearOpMode
 {
     DcMotor driveL;
     DcMotor driveR;
+    DcMotor duck;
+    DcMotor arm;
+    DcMotor intake;
     
     // do i need to make this public? java is weird idk
     // moves drive motors the specified number of ticks and does telemetry stuff
@@ -50,6 +53,11 @@ public class vroom extends LinearOpMode
     {
         driveL = hardwareMap.dcMotor.get("driveL");
         driveR = hardwareMap.dcMotor.get("driveR");
+        duck = hardwareMap.dcMotor.get("duck");
+        arm = hardwareMap.dcMotor.get("arm");
+        intake = hardwareMap.dcMotor.get("intake");
+        
+        // AYO DO I NEED TO setZeroPowerBehavior????
 
         // may or may not need depending on the robot
         //driveL.setDirection(DcMotor.Direction.REVERSE);
@@ -66,6 +74,43 @@ public class vroom extends LinearOpMode
         telemetry.addData("Status", "Running");
         telemetry.update();
         
+        // actual program
+        driveToTarget(5000, 5000, 0.8, 0.8) //forwards to carousel (completely arbitrary numbers btw)
         
+        // carousel
+        duck.setPower(1.0)
+        sleep(3000)
+        duck.setPower(0.0)
+        
+        driveToTarget(-10000, -10000, 1.0, 1.0) //backwards to around the middle
+        
+        driveToTarget(0, -2000, 0.0, 0.5) //turn towards hub
+        
+        driveToTarget(5000, 5000, 0.7, 0.7) //go towards hub
+        
+        // lift arm (i know, it could be done at the same time with driving, but a) lazy, and b) we have extra time for sure)
+        arm.setPower(1.0)
+        sleep(500)
+        arm.setPower(0.0) //this should brake the arm...
+        
+        driveToTarget(800, 800, 0.2, 0.2) //go towards hub even more
+        
+        // drop preload box
+        intake.setPower(-1.0)
+        sleep(300)
+        arm.setPower(0.0)
+        
+        driveToTarget(-800, -800, 0.2, 0.2) //away from hub
+        
+        // drop arm
+        arm.setPower(-1.0)
+        sleep(500)
+        arm.setPower(0.0)
+        
+        driveToTarget(-5100, -5100, 1.0, 1.0) //go to wall, then a bit more to square with wall
+        
+        driveToTarget(2000, 0, 0.5, 0.0) //turn towards warehouse
+        
+        driveToTarget(5000, 5000, 0.8, 0.8) //go into warehouse hopefully
     }
 }
