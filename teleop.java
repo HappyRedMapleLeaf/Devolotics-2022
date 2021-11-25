c6af06a36e94b47332f83fec82b7e9e15e70bbc5
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2021-11-24 21:13:46 -0500

    some small changes added, this should work...

--- teleop.java
+++ teleop.java
@@ -6,66 +6,63 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Pushbot: Teleop- BAY MODE ", group="Pushbot")
+@TeleOp(name="Bay POV", group="Devolotics")
 
-public class NewTeleOp extends LinearOpMode {
-
-    HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
+public class teleop extends LinearOpMode {
+    
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+    public DcMotor armMotor;
+    public DcMotor intakeMotor;
+    //public DcMotor duckMotor;
 
     @Override
     public void runOpMode() {
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        intakeMotor = hardwareMap.dcMotor.get("intake");
+        //duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        
         int armMin = 20; //0
         int armMax = 440; //456
         int armFreezeTarget = 0; //where the arm wants to stay stationary
 
-        /* Initialize the hardware variables.
-           The init() method of the hardware class does all the work here */
-        robot.init(hardwareMap);
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Bay the Gamer");
+        telemetry.addData("Hello", "Bay the Gamer");
         telemetry.update();
-        
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+        telemetry.addData("Status:", "Gaming");
+        telemetry.update();
         
+        //motor settings
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setDirection(DcMotor.Direction.REVERSE);
         
-        //robot.leftDrive.setDirection(DcMotor.Direction.REVERSE); // reversing left motor because robot dumdum
-        robot.armMotor.setDirection(DcMotor.Direction.REVERSE);
-        robot.armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         
-        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        
-        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-        
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             
-            // apparently bay wants some weird controls but he asked for it so.....
-            // left controls all movement, right controls turning only
-            
             if (Math.abs(gamepad1.right_stick_x) > 0.05) { //right stick is touched (some tolerance is given)
-                
-                // only turn
-                //one wheel, other wheel is stationary
-                //robot.leftDrive.setPower(Math.max(gamepad1.right_stick_x, 0));
-                //robot.rightDrive.setPower(Math.max(-gamepad1.right_stick_x, 0));
-                
-                //both wheels
-                robot.leftDrive.setPower(-gamepad1.right_stick_x);
-                robot.rightDrive.setPower(gamepad1.right_stick_x);
+                //turn
+                leftDrive.setPower(gamepad1.right_stick_x);
+                rightDrive.setPower(-gamepad1.right_stick_x);
                 
             } else { //right stick is untouched
                 
-                //if bay actually doesn't want left stick sideways movement to stop other movement, then uncomment the code below
-                double drive = gamepad1.left_stick_y;
+                double drive = -gamepad1.left_stick_y;
                 double turn  = gamepad1.left_stick_x;
                 // Combine drive and turn for blended motion.
-                double left  = drive - turn;
-                double right = drive + turn;
+                double left  = drive + turn;
+                double right = drive - turn;
 
                 // Normalize the values so neither exceed +/- 1.0
                 left = Math.min(left, 1.0);
@@ -73,25 +70,33 @@ public class NewTeleOp extends LinearOpMode {
                 right = Math.min(right, 1.0);
                 right = Math.max(right, -1.0);
 
-                robot.leftDrive.setPower(left);
-                robot.rightDrive.setPower(right);
-                
+                leftDrive.setPower(left);
+                rightDrive.setPower(right);
             }
             
             //intake motor
-            robot.intakeMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger); //left is out, right is in
+            intakeMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger); //left is out, right is in
             
+			/*
+            //duck motor
+			if gamepad1.dpad_up || gamepad1.dpad_left {
+				duckMotor.setPower(-1.0);
+			} else if gamepad1.dpad_down || gamepad1.dpad_right {
+				duckMotor.setPower(1.0);
+			} else {
+				duckMotor.setPower(0.0);
+			}
+			*/
             
-            //arm stuff
-            int armPosition = robot.armMotor.getCurrentPosition(); //reduces clutter
+            //arm movement
+            int armPosition = armMotor.getCurrentPosition();
             double armTargetPower = 0;
             
-            if (gamepad1.left_bumper) { //arm manual movement
-                //down
+            //manual movement
+            if (gamepad1.left_bumper) { //down
                 armTargetPower = -0.3;
-                armFreezeTarget = -1000;
-            } else if (gamepad1.right_bumper) {
-                //up
+                armFreezeTarget = -1000; //explained later
+            } else if (gamepad1.right_bumper) { //up
                 armTargetPower = 0.3;
                 armFreezeTarget = -1000;
             } else {
@@ -99,44 +104,43 @@ public class NewTeleOp extends LinearOpMode {
             }
             
             if (armTargetPower > 0) { //trying to move up
-                if (armPosition > armMax) {
+                if (armPosition > armMax) { //but too high
                     armTargetPower = 0; //stop
-                } else if (armPosition > armMax - 100) {
+                } else if (armPosition > armMax - 100) { //almost too high
                     armTargetPower = 0.2; //slow down
                 }
             } else if (armTargetPower < 0) { //trying to move down
-                if (armPosition < armMin + 100) {
-                    armTargetPower = 0; //slow down (really just stopping it)
+                if (armPosition < armMin + 100) { //almost too low
+                    armTargetPower = 0; //"stopping" the motor, but really, this just drops it slowly
                 }
             } else {
-                //kinda complex: armfreezetarget is only set for the first time that targetpower is 0
-                //this is done by setting freezetarget to -1000 whenever a non-zero target power is set
+                //kinda complex: armFreezeTarget is only set for the *first time* that targetpower is 0
+                //this is done by setting armFreezeTarget to -1000 whenever a non-zero target power is set
+                //so if the next loop comes and sees that armFreezeTarget isn't -1000, it knows not to change it
+                //because otherwise the "target" would keep moving and be useless
                 if (armFreezeTarget == -1000) {
                     armFreezeTarget = armPosition;
                 }
             }
             
-            if (gamepad1.a) {
-                armFreezeTarget = 180; //tbd
-            }
-            if (gamepad1.x) {
-                armFreezeTarget = 290; //tbd
-            }
+            //if the preset buttons are pressed, the freeze target changes
+            if (gamepad1.a) { armFreezeTarget = 180; }
+            if (gamepad1.x) { armFreezeTarget = 290; }
             
-            if (armFreezeTarget != -1000) {
+            //making sure that the arm is at the freeze target
+            if (armFreezeTarget != -1000) { //it shouldn't try going to the target if a non-zero target power is set
                 if (armPosition > armFreezeTarget) {
-                    robot.armMotor.setPower(0);
+                    armMotor.setPower(0); //stop the arm if it's too high, and let it fall
                 } else if (armPosition < armFreezeTarget) {
-                    robot.armMotor.setPower(0.12);
+                    armMotor.setPower(0.12); //slowly raise the arm if it's too low
                 }
             } else {
-                robot.armMotor.setPower(armTargetPower);
+                armMotor.setPower(armTargetPower);
             }
             
-            // Send telemetry message to show armMotor;
+            // Send telemetry
             telemetry.addData("armPosition", armPosition);
             telemetry.addData("freezeTarget", armFreezeTarget);
-            
             telemetry.update();
         }
     }
