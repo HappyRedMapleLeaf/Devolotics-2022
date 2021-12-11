0fed93f104608491d1022d48d0dd7d5032230c05
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2021-12-11 13:31:35 -0500

    Update RangeSensor.java

--- RangeSensor.java
+++ RangeSensor.java
@@ -9,13 +9,19 @@ public class SensorMRRangeSensor extends LinearOpMode {
 
    
         waitForStart();
-
-        while (opModeIsActive()) {
-            telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic()); // probably will only need this
-            telemetry.addData("raw optical", rangeSensor.rawOptical());
-            telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
-            telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM)); // probably will only need this
-            telemetry.update();
+        
+        while (robot.rangeSensor.getDistance(DistanceUnit.INCH) > )//distance from shipping element when driving along wall
+        {
+            telemetry.addData("cm", "%.2f cm", robot.rangeSensor.getDistance(DistanceUnit.INCH));
+            telemetry.update();// prints data
         }
+//         while (opModeIsActive()) {
+//             telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic()); 
+//             telemetry.addData("raw optical", rangeSensor.rawOptical());
+//             telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+//             telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM)); // probably will only need this
+//             telemetry.update();
+            
+//         }
     }
 }
