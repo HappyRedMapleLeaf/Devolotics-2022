d4ca45027a533d2a359ad90ed120f9b36bc3018a
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2021-12-15 15:25:05 -0500

    un dumb-ed

--- auton.java
+++ auton.java
@@ -1,4 +1,87 @@
-// actual program. Intial position: halfway between first and second ground tile
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Autonomous 2", group="Devolotics")
+
+public class auton extends LinearOpMode {
+    
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+    public DcMotor armMotor;
+    public DcMotor intakeMotor;
+    public DcMotor duckMotor;
+
+    // moves drive motors the specified number of ticks and does telemetry stuff
+    
+    // 1440 ticks when 60:1, somehow 720 when 40:1
+    public int ticksPerRotation = 1000;
+    
+    public void driveToTarget(double rotationL, double rotationR, double powerL, double powerR) {
+        // set targets for motors
+        int targetL = Math.toIntExact(Math.round(leftDrive.getCurrentPosition() + rotationL * ticksPerRotation));
+        int targetR = Math.toIntExact(Math.round(rightDrive.getCurrentPosition() + rotationR * ticksPerRotation));
+        leftDrive.setTargetPosition(targetL);
+        rightDrive.setTargetPosition(targetR);
+        
+        // tell motors that they're supposed to go to the target
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        // set power of motors. this is when they actually start to move
+        leftDrive.setPower(Math.abs(powerL));
+        rightDrive.setPower(Math.abs(powerR));
+        
+        // do telemetry stuff and also wait while the motors are turning
+        while (opModeIsActive() && (leftDrive.isBusy() || rightDrive.isBusy()))
+        {
+            telemetry.addData("Target",  "Running to %7d :%7d", (leftDrive.getCurrentPosition() + targetL),  (rightDrive.getCurrentPosition() + targetR));
+            telemetry.addData("Position",  "Running at %7d :%7d",
+                                        leftDrive.getCurrentPosition(),
+                                        rightDrive.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        
+        // stops the motor
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+        
+        // Turn off RUN_TO_POSITION
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        intakeMotor = hardwareMap.dcMotor.get("intake");
+        duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        
+        //motor settings
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        // start sequence
+        telemetry.addData("Status:", "Waiting for start");
+        telemetry.update();
+        waitForStart();
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+        
+        
+        // actual program. Intial position: halfway between first and second ground tile
         driveToTarget(0.76, 0.56, 1.0, 0.4); //forwards to carousel, turn a bit because duck wheel will hit the metal thingy
         
         
@@ -25,8 +108,8 @@
         // lift arm (i know, it could be done at the same time with driving, but a) lazy, and b) we have extra time for sure)
         armMotor.setPower(-0.5);
         //sleep(200); //level 1
-        //sleep(450); //level 2
-        sleep(950); //level 3
+        sleep(450); //level 2
+        //sleep(700); //level 3
         armMotor.setPower(-0.1); //too lazy to do encoder stuff, so im just gonna uhm... apply a little power and hope it keeps it up lol
         
         driveToTarget(0.31, 0.31, 0.2, 0.2); //go towards hub even more
@@ -50,3 +133,5 @@
         intakeMotor.setPower(-0.9);
         sleep(1000);
         intakeMotor.setPower(0.0);
+    }
+}
