9b16a763da53cc33d5374e6c2425c4bdc27c35ea
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2021-12-15 15:27:07 -0500

    added dumb
    dumb

--- RangeSensor.java
+++ RangeSensor.java
@@ -1,39 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="snesr!!1!", group="Devolotics")
+
 public class SensorTest extends LinearOpMode {
-
-    //ModernRoboticsI2cRangeSensor rangeSensor; // initialize
-    DistanceSensor rangeLeft;
     
+    DistanceSensor rangeLeft;
 
-    @Override public void runOpMode() {
-
-        //rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range_sensor");
-        rangeLeft = hardwareMap.get(DistanceSensor.class, "rangeL");
-   
-        waitForStart();
+    @Override
+    public void runOpMode() {
         
+
+        telemetry.addData("Hello", "Bay the Gamer");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        telemetry.addData("Status:", "Gaming");
+        telemetry.update();
+
+        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             telemetry.addData("range", String.format("%.01f mm", rangeLeft.getDistance(DistanceUnit.MM)));
-        }
-        
-        /*
-        
-        //Basically need to drive a certain distance check if there is an obstruction, 3x, from left to right. Each peariod will be assigned an integer, 
-        //and if it detects an obstruction 
-        //it will run the code for the appropriate shipping hub tier.
-        while (robot.rangeSensor.getDistance(DistanceUnit.INCH) > )//distance from shipping element when driving along wall
-        {
-            telemetry.addData("cm", "%.2f cm", robot.rangeSensor.getDistance(DistanceUnit.INCH));
-            telemetry.update();// prints data
-        }
-//         while (opModeIsActive()) {
-//             telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic()); 
-//             telemetry.addData("raw optical", rangeSensor.rawOptical());
-//             telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
-//             telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM)); // probably will only need this
-//             telemetry.update();
             
-//         }
-        
-        */
+        }
     }
 }
