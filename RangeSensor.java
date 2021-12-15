f3393373e483dd8d55f3c25d81f98130721c5585
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2021-12-14 23:57:26 -0500

    testing

--- RangeSensor.java
+++ RangeSensor.java
@@ -1,14 +1,22 @@
-public class SensorMRRangeSensor extends LinearOpMode {
+public class SensorTest extends LinearOpMode {
 
-    ModernRoboticsI2cRangeSensor rangeSensor; // initialize
+    //ModernRoboticsI2cRangeSensor rangeSensor; // initialize
+    DistanceSensor rangeLeft;
+    
 
     @Override public void runOpMode() {
 
-        // get a reference to compass
-        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range_sensor");
-
+        //rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range_sensor");
+        rangeLeft = hardwareMap.get(DistanceSensor.class, "rangeL");
    
         waitForStart();
+        
+        while (opModeIsActive()) {
+            telemetry.addData("range", String.format("%.01f mm", rangeLeft.getDistance(DistanceUnit.MM)));
+        }
+        
+        /*
+        
         //Basically need to drive a certain distance check if there is an obstruction, 3x, from left to right. Each peariod will be assigned an integer, 
         //and if it detects an obstruction 
         //it will run the code for the appropriate shipping hub tier.
@@ -25,5 +33,7 @@ public class SensorMRRangeSensor extends LinearOpMode {
 //             telemetry.update();
             
 //         }
+        
+        */
     }
 }
