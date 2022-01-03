57cdf3853d7edf859660b51c92be74d3171577d1
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2022-01-02 19:25:53 -0500

    Update RangeSensor.java

--- RangeSensor.java
+++ RangeSensor.java
@@ -4,12 +4,12 @@ import edu.wpi.first.wpilibj.AnalogInput;
 public AnalogInput sharp;
 
 //Constuct a new instance
-sharp = new AnalogInput(port);
-
+// sharp = new AnalogInput(port); // still have to figure this out
+rangeSensor = hardwareMap.DeviceMapping("rangeSensor") // this - idk if this works
 //Create an accessor method
 public double range = getDistance()
 {
-    return (Math.pow(sharp.getAverageVoltage(), -1.2045)) * 27.726;
+    return (Math.pow(sharp.getAverageVoltage(), -1.2045)) * 27.726; // The accessor method will output the range in cm.
 }
 
  while (opModeIsActive()) {
