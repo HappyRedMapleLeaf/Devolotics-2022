f9fba13f448b480aece0724832b396aea9bf5ded
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2022-01-02 19:18:33 -0500

    Update RangeSensor.java

--- RangeSensor.java
+++ RangeSensor.java
@@ -1,32 +1,13 @@
-package org.firstinspires.ftc.teamcode;
+import edu.wpi.first.wpilibj.AnalogInput;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
+//Create the Analog Object
+public AnalogInput sharp;
 
-@TeleOp(name="snesr!!1!", group="Devolotics")
+//Constuct a new instance
+sharp = new AnalogInput(port);
 
-public class SensorTest extends LinearOpMode {
-    
-    DistanceSensor rangeLeft;
-
-    @Override
-    public void runOpMode() {
-        
-
-        telemetry.addData("Hello", "Bay the Gamer");
-        telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        telemetry.addData("Status:", "Gaming");
-        telemetry.update();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("range", String.format("%.01f mm", rangeLeft.getDistance(DistanceUnit.MM)));
-            
-        }
-    }
+//Create an accessor method
+public double getDistance()
+{
+    return (Math.pow(sharp.getAverageVoltage(), -1.2045)) * 27.726;
 }
