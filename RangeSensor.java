8a2b42698db4177321c61ed6ac991ecfb0afac51
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2022-01-02 19:33:42 -0500

    Update RangeSensor.java

--- RangeSensor.java
+++ RangeSensor.java
@@ -5,8 +5,10 @@ public AnalogInput sharp;
 public DeviceMapping(java.lang.Class<DEVICE_TYPE> deviceTypeClass) //not sure what device_type is
 //Constuct a new instance
 // sharp = new AnalogInput(port); // still have to figure this out
-rangeSensor = hardwareMap.DeviceMapping("rangeSensor") // this - idk if this works
-//Create an accessor method
+public {
+    rangeSensor = HardwareMap.DeviceMapping<DEVICE_TYPE extends HardwareDevice> // this - idk if this works
+    }
+        //Create an accessor method
 public double range = getDistance()
 {
     return (Math.pow(sharp.getAverageVoltage(), -1.2045)) * 27.726; // The accessor method will output the range in cm.
