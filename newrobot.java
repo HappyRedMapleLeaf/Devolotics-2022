ccb00c401651b11515ee49da63223a13cacf3e29
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2022-01-02 19:04:48 -0500

    Update newrobot.java
    try this

--- newrobot.java
+++ newrobot.java
@@ -20,13 +20,14 @@ public class teleop extends LinearOpMode {
     public void runOpMode() {
         leftDrive = hardwareMap.dcMotor.get("leftDrive");
         rightDrive = hardwareMap.dcMotor.get("rightDrive");
-        armMotor = hardwareMap.dcMotor.get("armMotor");
+        armMotor1 = hardwareMap.dcMotor.get("armMotor1");
+        armMotor2 = hardwareMap.dcMotor.get("armMotor2");
         intakeMotor = hardwareMap.dcMotor.get("intake");
         duckMotor = hardwareMap.dcMotor.get("duckMotor");
         
-        int armMin = 20; //0
-        int armMax = 455; //456
-        int armFreezeTarget = 0; //where the arm wants to stay stationary
+//         int armMin = 20; //0
+//         int armMax = 455; //456
+//         int armFreezeTarget = 0; //where the arm wants to stay stationary
 
         telemetry.addData("Hello", "Bay the Gamer");
         telemetry.update();
@@ -39,16 +40,18 @@ public class teleop extends LinearOpMode {
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         armMotor.setDirection(DcMotor.Direction.REVERSE);
         
-        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+//         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // run until the end of the match (driver presses STOP)
+        
         while (opModeIsActive()) {
             
             if (Math.abs(gamepad1.right_stick_x) > 0.05) { //right stick is touched (some tolerance is given)
@@ -78,60 +81,31 @@ public class teleop extends LinearOpMode {
             } else {
                 duckMotor.setPower(0.0);
             }
-            
+
             
             //arm movement
-            int armPosition = armMotor.getCurrentPosition();
+            int armPosition1 = armMotor1.getCurrentPosition();
+            int armPosition2 = armMotor2.getCurrentPosition();
             double armTargetPower = 0;
             
-            //manual movement
-            if (gamepad1.left_bumper) { //down
-                armTargetPower = -0.3;
-                armFreezeTarget = -1000; //explained later
-            } else if (gamepad1.right_bumper) { //up
-                armTargetPower = 0.3;
-                armFreezeTarget = -1000;
+            
+            if (gamepad1.left_bumper) {
+                armMotor1.setPower(-0.7);
+                armMotor2.setPower(-0.7);
+            } else if (gamepad1.right_bumper) {
+                armMotor1.setPower(0.7);
+                armMotor2.setPower(0.7);
             } else {
-                armTargetPower = 0;
+                armMotor1.setPower(0);
+                armMotor2.setPower(0);
             }
             
-            if (armTargetPower > 0) { //trying to move up
-                if (armPosition > armMax) { //but too high
-                    armTargetPower = 0; //stop
-                } else if (armPosition > armMax - 100) { //almost too high
-                    armTargetPower = 0.2; //slow down
-                }
-            } else if (armTargetPower < 0) { //trying to move down
-                if (armPosition < armMin + 100) { //almost too low
-                    armTargetPower = 0; //"stopping" the motor, but really, this just drops it slowly
-                }
-            } else {
-                //kinda complex: armFreezeTarget is only set for the *first time* that targetpower is 0
-                //this is done by setting armFreezeTarget to -1000 whenever a non-zero target power is set
-                //so if the next loop comes and sees that armFreezeTarget isn't -1000, it knows not to change it
-                //because otherwise the "target" would keep moving and be useless
-                if (armFreezeTarget == -1000) {
-                    armFreezeTarget = armPosition;
-                }
-            }
             
-            //if the preset buttons are pressed, the freeze target changes
-            if (gamepad1.a) { armFreezeTarget = 180; }
-            if (gamepad1.x) { armFreezeTarget = 290; }
-            
-            //making sure that the arm is at the freeze target
-            if (armFreezeTarget != -1000) { //it shouldn't try going to the target if a non-zero target power is set
-                if (armPosition > armFreezeTarget) {
-                    armMotor.setPower(0); //stop the arm if it's too high, and let it fall
-                } else if (armPosition < armFreezeTarget) {
-                    armMotor.setPower(0.12); //slowly raise the arm if it's too low
-                }
-            } else {
-                armMotor.setPower(armTargetPower);
-            }
+     
             
             // Send telemetry
-            telemetry.addData("armPosition", armPosition);
+            telemetry.addData("armPosition1", armPosition1);
+            telemetry.addData("armPosition2", armPosition2);
             telemetry.addData("freezeTarget", armFreezeTarget);
             telemetry.update();
         }
