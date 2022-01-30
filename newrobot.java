cee7ad1da25d21e41c358d18a4008a93fed41593
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2022-01-30 17:14:39 -0500

    literally basically reverted to old code

--- newrobot.java
+++ newrobot.java
@@ -4,55 +4,48 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Nenjia Pov", group="Devolotics")
+@TeleOp(name="Nengjia POV", group="Devolotics")
 
-public class Nenjiadumbbot extends LinearOpMode {
+public class Nengjiabeingdumb extends LinearOpMode {
     
     public DcMotor leftDrive;
     public DcMotor rightDrive;
-    public DcMotor armMotor1;
-    public DcMotor armMotor2;
-    //public DcMotor duckMotor;
-
+    public DcMotor armMotor;
+    public DcMotor duckMotor;
+    public DcMotor intakeMotor;
+    
     @Override
     public void runOpMode() {
         leftDrive = hardwareMap.dcMotor.get("leftDrive");
         rightDrive = hardwareMap.dcMotor.get("rightDrive");
-        armMotor1 = hardwareMap.dcMotor.get("armMotor1");
-        armMotor2 = hardwareMap.dcMotor.get("armMotor2");
-        //duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        intakeMotor = hardwareMap.dcMotor.get("intake");
 
-        telemetry.addData("Hello", "Bay the Gamer");
+        telemetry.addData("Hello", "Nengjia the carry");
         telemetry.update();
-        //Wait for the game to start (driver presses PLAY)
         waitForStart();
-        telemetry.addData("Status:", "Gaming");
+        telemetry.addData("Status:", "Getting assasinated by robot");
         telemetry.update();
         
         //motor settings
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        armMotor1.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setDirection(DcMotor.Direction.REVERSE);
         
-        armMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        
-        final int armMin = 20; //0
-        final int armMax = 360; //380
-        final int armTop = 190;
-        final int armSpeed = 5;
-        int armTarget = 0;
-        
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		
+        int armMin = 0;
+        int armMax = 420;
+        int armTarget = -1000; //where the arm wants to stay stationary
+
         while (opModeIsActive()) {
             
             if (Math.abs(gamepad1.right_stick_x) > 0.05) { //right stick is touched (some tolerance is given)
@@ -64,19 +57,17 @@ public class Nenjiadumbbot extends LinearOpMode {
                 leftDrive.setPower(-gamepad1.left_stick_y);
                 rightDrive.setPower(-gamepad1.left_stick_y);
             }
+			
             
             //intake motor
-            /*
             if (gamepad1.left_trigger > 0){
                 intakeMotor.setPower(0.9);
             } else if (gamepad1.right_trigger > 0){
                 intakeMotor.setPower(-0.7);
             }
             intakeMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger); //left is out, right is in
-            */
             
             //duck motor, only need one direction
-            /*
             if ( gamepad1.dpad_left || gamepad1.dpad_right) {
                 duckMotor.setPower(-0.4);
             } else if ( gamepad1.dpad_down || gamepad1.dpad_up ){
@@ -84,38 +75,42 @@ public class Nenjiadumbbot extends LinearOpMode {
             } else {
                 duckMotor.setPower(0.0);
             }
-            */
-
+            
             
             //arm movement
-            int armPosition = (armMotor1.getCurrentPosition() + armMotor2.getCurrentPosition()) / 2;
+            int armPosition = armMotor.getCurrentPosition();
+            double armPower = 0;
             
             if (gamepad1.left_bumper) {             //down
-                armTarget -= armSpeed;
-                if (armTarget < armMin) {armTarget = armMin;}
+				armTarget = -1000;
+				if (armPosition < armMin + 100) {
+                    armPower = 0; //if almost too low then slow down
+                } else {
+					armPower = -0.2; //usual speed
+				}
+
             } else if (gamepad1.right_bumper) {     //up
-                armTarget += armSpeed;
-                if (armTarget > armMax) {armTarget = armMax;}
-            }
-            
-            //gravity is dumb. if this code is too complicated blame gravity and newton and einstein.
-            if (armPosition >= armTop) {
-                if (armPosition >= armTarget) {
-                    armMotor1.setPower(-((armPosition - armTarget) * (armPosition - armTop)) / 36100);
-                    armMotor2.setPower(-((armPosition - armTarget) * (armPosition - armTop)) / 36100);
+				armTarget = -1000;
+				if (armPosition > armMax) {
+                    armPower = 0; //if too high then stop
+                } else if (armPosition > armMax - 100) {
+                    armPower = 0.15; //if almost too high slow down
                 } else {
-                    armMotor1.setPower(0);
-                    armMotor2.setPower(0);
-                }
+					armPower = 0.3; //usual speed
+				}
+
             } else {
-                if (armPosition >= armTarget) {
-                    armMotor1.setPower(0);
-                    armMotor2.setPower(0);
+				if (armTarget == -1000) {
+					armTarget = armPosition;
+				}
+				if (armPosition >= armTarget) {
+                    armPower = 0; //stop the arm if it's too high, and let it fall
                 } else {
-                    armMotor1.setPower(((armPosition - armTarget) * (armPosition - armTop)) / 36100);
-                    armMotor2.setPower(((armPosition - armTarget) * (armPosition - armTop)) / 36100);
+                    armPower = 0.12; //slowly raise the arm if it's too low
                 }
-            }
+			}
+            
+			armMotor.setPower(armPower);
             
             // Send telemetry
             telemetry.addData("armPosition", armPosition);
