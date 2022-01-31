fbcc63ee57a9e85e9a0771eee5c3fd2511ec4f6b
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2022-01-31 18:39:36 -0500

    changed duck to servo
    also fixed indents... again...

--- newrobot.java
+++ newrobot.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="ProvsTeleop", group="Devolotics")
 
@@ -12,16 +13,16 @@ public class Nengjiabeingdumb extends LinearOpMode {
 	public DcMotor leftDrive;
 	public DcMotor rightDrive;
 	public DcMotor armMotor;
-	public DcMotor duckMotor;
 	public DcMotor intakeMotor;
+	public Servo duckMotor;
 
 	@Override
 	public void runOpMode() {
 	leftDrive = hardwareMap.dcMotor.get("leftDrive");
 	rightDrive = hardwareMap.dcMotor.get("rightDrive");
 	armMotor = hardwareMap.dcMotor.get("armMotor");
-	duckMotor = hardwareMap.dcMotor.get("duckMotor");
 	intakeMotor = hardwareMap.dcMotor.get("intake");
+	duckMotor = hardwareMap.Servo.get("duckMotor");
 
 	telemetry.addData("Hello", "Nengjia the carry");
 	telemetry.update();
@@ -45,72 +46,70 @@ public class Nengjiabeingdumb extends LinearOpMode {
 	int armTarget = -1000; //where the arm wants to stay stationary
 
 	while (opModeIsActive()) {
+		//drive
+		if (Math.abs(gamepad1.right_stick_x) > 0.05) { //right stick is touched (some tolerance is given)
+			//turn
+			leftDrive.setPower(gamepad1.right_stick_x);
+			rightDrive.setPower(-gamepad1.right_stick_x);
+		} else { //right stick is untouched
+		leftDrive.setPower(-gamepad1.left_stick_y);
+		rightDrive.setPower(-gamepad1.left_stick_y);
+		}
 
-		    if (Math.abs(gamepad1.right_stick_x) > 0.05) { //right stick is touched (some tolerance is given)
-				//turn
-				leftDrive.setPower(gamepad1.right_stick_x);
-				rightDrive.setPower(-gamepad1.right_stick_x);
-		    } else { //right stick is untouched
-			leftDrive.setPower(-gamepad1.left_stick_y);
-			rightDrive.setPower(-gamepad1.left_stick_y);
-		    }
+		//intake
+		if (gamepad1.left_trigger > 0){
+		intakeMotor.setPower(0.9);
+		} else if (gamepad1.right_trigger > 0) {
+		intakeMotor.setPower(-0.7);
+		}
+		intakeMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger); //left is out, right is in
 
+		//duck
+		if ( gamepad1.dpad_left || gamepad1.dpad_right) {
+		duckMotor.setPosition(0.3);
+		} else if ( gamepad1.dpad_down || gamepad1.dpad_up ) {
+		duckMotor.setPower(0.0);
+		} else {
+		duckMotor.setPower(0.5);
+		}
 
-		    //intake motor
-		    if (gamepad1.left_trigger > 0){
-			intakeMotor.setPower(0.9);
-		    } else if (gamepad1.right_trigger > 0) {
-			intakeMotor.setPower(-0.7);
-		    }
-		    intakeMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger); //left is out, right is in
+		//arm
+		int armPosition = armMotor.getCurrentPosition();
+		double armPower = 0;
 
-		    //duck motor, only need one direction
-		    if ( gamepad1.dpad_left || gamepad1.dpad_right) {
-			duckMotor.setPower(-0.4);
-		    } else if ( gamepad1.dpad_down || gamepad1.dpad_up ) {
-			duckMotor.setPower(-1.0);
-		    } else {
-			duckMotor.setPower(0.0);
-		    }
-
-
-		    //arm movement
-		    int armPosition = armMotor.getCurrentPosition();
-		    double armPower = 0;
-
-		    if (gamepad1.left_bumper) {             //down
-			    armTarget = -1000;
-			    if (armPosition < armMin + 100) {
-					armPower = 0; //if almost too low then slow down
-				} else {
-					armPower = -0.2; //usual speed
-				}
-			} else if (gamepad1.right_bumper) {     //up
-				armTarget = -1000;
-				if (armPosition > armMax) {
-					armPower = 0; //if too high then stop
-				} else if (armPosition > armMax - 100) {
-					armPower = 0.15; //if almost too high slow down
-				} else {
-					armPower = 0.3; //usual speed
-				}
+		if (gamepad1.left_bumper) {             //down
+		        armTarget = -1000;
+		        if (armPosition < armMin + 100) {
+				armPower = 0; //if almost too low then slow down
 			} else {
-				if (armTarget == -1000) {
-					armTarget = armPosition;
-				}
-				if (armPosition >= armTarget) {
-					armPower = 0; //stop the arm if it's too high, and let it fall
-				} else {
-					armPower = 0.12; //slowly raise the arm if it's too low
-				}
+				armPower = -0.2; //usual speed
 			}
+		} else if (gamepad1.right_bumper) {     //up
+			armTarget = -1000;
+			if (armPosition > armMax) {
+				armPower = 0; //if too high then stop
+			} else if (armPosition > armMax - 100) {
+				armPower = 0.15; //if almost too high slow down
+			} else {
+				armPower = 0.3; //usual speed
+			}
+		} else {
+			if (armTarget == -1000) {
+				armTarget = armPosition;
+			}
+			if (armPosition >= armTarget) {
+				armPower = 0; //stop the arm if it's too high, and let it fall
+			} else {
+				armPower = 0.12; //slowly raise the arm if it's too low
+			}
+		}
 
-			armMotor.setPower(armPower);
+		armMotor.setPower(armPower);
 
-		    // Send telemetry
-		    telemetry.addData("armPosition", armPosition);
-		    telemetry.addData("armTarget", armTarget);
-		    telemetry.update();
+		// Send telemetry
+		telemetry.addData("armPosition", armPosition);
+		telemetry.addData("armTarget", armTarget);
+		telemetry.update();
 		}
 	}
 }
