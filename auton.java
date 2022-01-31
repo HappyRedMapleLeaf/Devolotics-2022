004acc55d75ae1c6d2afdfa47efa603f03a770ae
HappyRedMapleLeaf <92286231+HappyRedMapleLeaf@users.noreply.github.com>
2022-01-31 13:53:57 -0500

    will revert this later
    this is for screenshot for portfolio

--- auton.java
+++ auton.java
@@ -84,17 +84,13 @@ public class auton extends LinearOpMode {
         // actual program. Intial position: halfway between first and second ground tile
         driveToTarget(0.76, 0.56, 1.0, 0.4); //forwards to carousel, turn a bit because duck wheel will hit the metal thingy
         
-        
         // carousel
         duckMotor.setPower(-0.5);
         sleep(3000);
         duckMotor.setPower(0.0);
         
-        
         driveToTarget(-0.9, -0.83, 0.5, 0.4); //back to initial position
-        
         driveToTarget(-2.51, -2.58, 1.0, 1.0); //backwards to around the middle
-        
         driveToTarget(1.04, -1.04, 0.5, 0.5); //turn towards hub
         
         leftDrive.setPower(-0.5);
@@ -107,11 +103,8 @@ public class auton extends LinearOpMode {
         
         // lift arm (i know, it could be done at the same time with driving, but a) lazy, and b) we have extra time for sure)
         armMotor.setPower(-0.5);
-        //sleep(200); //level 1
         sleep(450); //level 2
-        //sleep(700); //level 3
-        armMotor.setPower(-0.1); //too lazy to do encoder stuff, so im just gonna uhm... apply a little power and hope it keeps it up lol
-        
+        armMotor.setPower(-0.1);
         driveToTarget(0.31, 0.31, 0.2, 0.2); //go towards hub even more
         
         // drop preload box
@@ -125,9 +118,7 @@ public class auton extends LinearOpMode {
         armMotor.setPower(0.0);
         
         driveToTarget(-1.6, -1.6, 1.0, 1.0); //go to wall, then a bit more to square with wall
-        
         driveToTarget(2.01, -0.03, 0.5, 0.1); //turn towards warehouse
-        
         driveToTarget(4, 4, 0.81, 0.8); //go into warehouse
         
         intakeMotor.setPower(-0.9);
